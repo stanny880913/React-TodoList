d6930f025da4a3c0c201296f62c2661114c2a23e
stanny880913 <stanny11122@gmail.com>
2023-11-19 12:15:42 +0800

    creat add item function and delete some unuse file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  //State Hook - useState
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleButtonClick() {
+    if(!newItem){
+      alert("Enter new items...");
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem,
+    };
+    setItems((oldList) => [...oldList, item]);
+    setNewItem("");
+    console.log(item);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Header */}
+      <h1>My Todolist</h1>
+      {/* Input(Input and Button) */}
+      <input
+        type="text"
+        placeholder="Add todo..."
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+      <button onClick={handleButtonClick}>Add</button>
+      {/* List of item (unordered list of list item) */}
+      <ul>
+        {items.map(item => {
+          return(
+            <li key={item.id}>{item.value}</li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
