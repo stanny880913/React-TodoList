a9f66347a3be81849ef4bf6177ba04ea16d66367
stanny880913 <stanny11122@gmail.com>
2023-11-20 18:11:04 +0800

    add delete and edit function and add css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import React, { useState } from "react";
 import "./App.css";
-import { useState } from "react";
 
 function App() {
-  //State Hook - useState
+  // State Hook - `useState`
   const [newItem, setNewItem] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleButtonClick() {
-    if(!newItem){
-      alert("Enter new items...");
+  const [showEdit, setShowEdit] = useState(-1);
+  const [updatedText, setUpdatedText] = useState("");
+
+  // Helper Functions
+
+  /* Adds a new item to the list array*/
+  function addItem() {
+    // ! Check for empty item
+    if (!newItem) {
+      alert("Press enter an item.");
       return;
     }
 
@@ -16,28 +23,85 @@ function App() {
       id: Math.floor(Math.random() * 1000),
       value: newItem,
     };
+
+    // Add new item to items array
     setItems((oldList) => [...oldList, item]);
+
+    // Reset newItem back to original state
     setNewItem("");
-    console.log(item);
   }
+
+  /* Deletes an item based on the `item.id` key */
+  function deleteItem(id) {
+    const newArray = items.filter((item) => item.id !== id);
+    setItems(newArray);
+  }
+
+  /* Edit an item text after creating it. */
+  function editItem(id, newText) {
+    // Get the current item
+    const currentItem = items.filter((item) => item.id === id);
+
+    // Create a new item with same id
+    const newItem = {
+      id: currentItem.id,
+      value: newText,
+    };
+
+    deleteItem(id);
+
+    // Replace item in the item list
+    setItems((oldList) => [...oldList, newItem]);
+    setUpdatedText("");
+    setShowEdit(-1);
+  }
+
+  // Main part of app
   return (
-    <div className="App">
-      {/* Header */}
-      <h1>My Todolist</h1>
-      {/* Input(Input and Button) */}
+    <div className="app">
+      {/* 1. Header  */}
+      <h1>My Todo List</h1>
+
+      {/* 2. Add new item (input) */}
       <input
         type="text"
-        placeholder="Add todo..."
+        placeholder="Add an item..."
         value={newItem}
         onChange={(e) => setNewItem(e.target.value)}
       />
-      <button onClick={handleButtonClick}>Add</button>
-      {/* List of item (unordered list of list item) */}
+
+      {/* Add (button) */}
+      <button onClick={() => addItem()}>Add</button>
+
+      {/* 3. List of todos (unordered list) */}
       <ul>
-        {items.map(item => {
-          return(
-            <li key={item.id}>{item.value}</li>
-          )
+        {items.map((item) => {
+          return (
+            <div>
+              <li key={item.id} onClick={() => setShowEdit(item.id)}>
+                {item.value}
+                <button
+                  className="delete-button"
+                  onClick={() => deleteItem(item.id)}
+                >
+                  ❌
+                </button>
+              </li>
+
+              {showEdit === item.id ? (
+                <div>
+                  <input
+                    type="text"
+                    value={updatedText}
+                    onChange={(e) => setUpdatedText(e.target.value)}
+                  />
+                  <button onClick={() => editItem(item.id, updatedText)}>
+                    Update
+                  </button>
+                </div>
+              ) : null}
+            </div>
+          );
         })}
       </ul>
     </div>
