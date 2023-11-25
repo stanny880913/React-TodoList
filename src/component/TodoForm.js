9e499aa31eb97adcf593302a5d02e0645c2193af
stanny880913 <stanny11122@gmail.com>
2023-11-26 01:08:06 +0800

    new code, add todo and remove todo function

---
+++ src/component/TodoForm.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+
+function TodoForm(props) {
+  const [input, setInput] = useState('');
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+
+    setInput("");
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a Todo"
+        value={input}
+        name="text"
+        className="todo-input"
+        onChange={handleChange}
+      ></input>
+      <button className="todo-button">Add Todo</button>
+    </form>
+  );
+}
+
+export default TodoForm;
---
+++ src/component/TodoList.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+  };
+
+  const removeTodo = id =>{
+    const removeArr = [...todos].filter(todo => todo.id !== id);
+
+    setTodos(removeArr);
+  }
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+        if(todo.id ===id){
+            todo.isComplete = !todo.isComplete
+        }
+        return todo
+    })
+    setTodos(updatedTodos)
+  }
+
+  return (
+    <div>
+      <h1>What's The Plan For Today ? </h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}/>
+    </div>
+  );
+}
+
+export default TodoList;
